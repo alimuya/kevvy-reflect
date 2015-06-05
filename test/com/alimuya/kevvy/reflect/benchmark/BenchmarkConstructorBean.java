7c07fda2de89c52d2a200fec4ec79a45fd7ace28
lixin <bule049@sina.com>
2015-06-05 14:55:02 +0800

    Constructor benchmark

---
+++ test/com/alimuya/kevvy/reflect/benchmark/BenchmarkConstructorBean.java
@@ -0,0 +1,11 @@
+package com.alimuya.kevvy.reflect.benchmark;
+
+
+public class BenchmarkConstructorBean {
+	private int a=2;
+	public BenchmarkConstructorBean (){}
+	
+	public BenchmarkConstructorBean (String p1, String p2,String p3,double p4,int p5){
+		
+	}
+}
--- test/com/alimuya/kevvy/reflect/benchmark/ConstructorAccessBenchmark.java
+++ test/com/alimuya/kevvy/reflect/benchmark/ConstructorAccessBenchmark.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Constructor;
 
 import com.alimuya.kevvy.reflect.KevvyConstructor;
 import com.alimuya.kevvy.reflect.KevvyConstructorReflect;
-import com.alimuya.kevvy.reflect.test.bean.TestConstructorBean;
 
 
 public class ConstructorAccessBenchmark extends Benchmark {
@@ -13,35 +12,70 @@ public class ConstructorAccessBenchmark extends Benchmark {
 		int count = 1000000;
 		Object[] dontCompileMeAway = new Object[count];
 
-		Class type = TestConstructorBean.class;
-		KevvyConstructorReflect<TestConstructorBean> constructorReflect = KevvyConstructorReflect.createKevvyConstructor(TestConstructorBean.class);
-		KevvyConstructor<TestConstructorBean> access = constructorReflect.getConstructor(String.class);
-		Constructor javac = type.getConstructor(String.class);
-
-		for (int i = 0; i < 100; i++)
-			for (int ii = 0; ii < count; ii++)
-				dontCompileMeAway[ii] = access.newInstance("sss");
-//				dontCompileMeAway[ii] =KevvyConstructorReflect.newIstanceWithoutConstructor(type);
-		for (int i = 0; i < 100; i++)
-			for (int ii = 0; ii < count; ii++)
-				dontCompileMeAway[ii] = javac.newInstance("sss");
+		Class<BenchmarkConstructorBean> type = BenchmarkConstructorBean.class;
+		KevvyConstructorReflect<BenchmarkConstructorBean> kevvyReflect = KevvyConstructorReflect.createKevvyConstructor(type);
+		KevvyConstructor<BenchmarkConstructorBean> kevvyNoParameter = kevvyReflect.getConstructor();
+		KevvyConstructor<BenchmarkConstructorBean> kevvyParameters = kevvyReflect.getConstructor(String.class, String.class,String.class,double.class,int.class);
+		Constructor<BenchmarkConstructorBean> javaNoParameter = type.getConstructor();
+		Constructor<BenchmarkConstructorBean> javaParameters = type.getConstructor(String.class, String.class,String.class,double.class,int.class);
+		
+		for (int i = 0; i < 100; i++){
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = kevvyNoParameter.newInstance();
+			}
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = javaNoParameter.newInstance();
+			}
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = kevvyParameters.newInstance("vv","ov","alimuya",3.14,1);
+			}
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = javaParameters.newInstance("vv","ov","alimuya",3.14,1);
+			}
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = KevvyConstructorReflect.newIstanceWithoutConstructor(BenchmarkConstructorBean.class);
+			}
+		}
 		warmup = false;
 
 		for (int i = 0; i < 100; i++) {
 			start();
-			for (int ii = 0; ii < count; ii++)
-				dontCompileMeAway[ii] = access.newInstance("sss");
-//				dontCompileMeAway[ii] =KevvyConstructorReflect.newIstanceWithoutConstructor(type);
-			end("Kevvy-Constructor");
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = kevvyNoParameter.newInstance();
+			}
+			end("Kevvy-No-Parameter");
 		}
 		for (int i = 0; i < 100; i++) {
 			start();
-			for (int ii = 0; ii < count; ii++)
-				dontCompileMeAway[ii] = javac.newInstance("sss");
-			end("java-Reflection");
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = javaNoParameter.newInstance();
+			}
+			end("Java-No-Parameter");
 		}
-
-		chart("Constructor");
+		
+		for (int i = 0; i < 100; i++) {
+			start();
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = kevvyParameters.newInstance("vv","ov","alimuya",3.14,1);
+			}
+			end("Kevvy-Parameters");
+		}
+		for (int i = 0; i < 100; i++) {
+			start();
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = javaParameters.newInstance("vv","ov","alimuya",3.14,1);
+			}
+			end("Java-Parameters");
+		}
+		for (int i = 0; i < 100; i++) {
+			start();
+			for (int ii = 0; ii < count; ii++){
+				dontCompileMeAway[ii] = KevvyConstructorReflect.newIstanceWithoutConstructor(BenchmarkConstructorBean.class);
+			}
+			end("Kevvy-Without-Constructor");
+		}
+		
+		chart("Kevvy Constructor");
 	}
 
 	public static void main (String[] args) throws Exception {
